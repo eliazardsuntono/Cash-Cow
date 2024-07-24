fb284e6d8ac64fe39a9ceb4078b9d18281d10dea
eliazardsuntono <eliazardsunton@gmail.com>
2024-07-24 15:30:47 -0400

    changed frontend dependencies and added the backend

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 var express = require("express");
 var parseurl = require("parseurl");
 var session = require("express-session");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var { Configuration, PlaidApi, PlaidEnvironments } = require("plaid");
 const redis = require("redis");
+var app = express();
 
 const RedisStore = require("connect-redis").default;
 var client = redis.createClient({
@@ -9,39 +13,13 @@ var client = redis.createClient({
 });
 
 (async () => { await client.connect(); })();
+app.use(cors());
+app.use(bodyParser.json());
+client.on("error", function (error) {
+    console.error(error);
+})
 
-var app = express();
-
-app.use(
-  session({
-    store: new RedisStore({ client: client }),
-    secret: "forest squirrel",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-
-app.use(function (req, res, next) {
-  if (!req.session.views) {
-    req.session.views = {};
-  }
-
-  // get the url pathname
-  var pathname = parseurl(req).pathname;
-
-  // count the views
-  req.session.views[pathname] = (req.session.views[pathname] || 0) + 1;
-  next();
+client.on("connect", function () {
+    console.log("Connected to Redis");
 });
 
-app.get("/foo", function (req, res, next) {
-  res.send("you viewed this page " + req.session.views["/foo"] + " times");
-});
-
-app.get("/bar", function (req, res, next) {
-  res.send("you viewed this page " + req.session.views["/bar"] + " times");
-});
-
-app.listen(3000, function () {
-  console.log("Example app listening on port 3000!");
-});
