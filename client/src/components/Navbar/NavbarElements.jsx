25486aad41f7e350164e1a6f9678c0f8499c56db
eliazardsuntono <eliazardsunton@gmail.com>
2024-07-30 13:27:10 -0400

    implemented beginning of dashboard

--- client/src/components/Navbar/NavbarElements.jsx
+++ client/src/components/Navbar/NavbarElements.jsx
@@ -3,7 +3,7 @@ import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Nav = styled.nav`
-  background: #63d471;
+  background: #33914C;
   height: 85px;
   display: flex;
   justify-content: center;
