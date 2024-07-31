4b59c84745eacd499d4042e7bf76ca0fc4dac0f0
Aashay Gupte <151937841+conterating@users.noreply.github.com>
2024-07-30 21:53:11 -0400

    Changed font style to Poppins and added font files

--- client/src/components/Navbar/NavbarElements.jsx
+++ client/src/components/Navbar/NavbarElements.jsx
@@ -3,7 +3,7 @@ import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Nav = styled.nav`
-  background: #33914C;
+  background: #33914c;
   height: 85px;
   display: flex;
   justify-content: center;
--- client/src/pages/rankings.jsx
+++ client/src/pages/rankings.jsx
@@ -1,5 +1,5 @@
 import styles from "./rankings.module.css";
-import RankingHeader from "./../components/ranking/RankingHeader";
+import RankingHeader from "../components/ranking/RankingHeader";
 
 export default function Rankings() {
   return <RankingHeader />;
